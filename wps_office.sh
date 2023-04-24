4d018acba3ec676d4ddef29193d261014896fe67
Bruno Finger <48925356+brunofinger@users.noreply.github.com>
2023-04-23 21:37:55 -0300

    Create wps_office.sh

---
+++ wps_office.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository ppa:wps-office/ppa
+sudo apt update
+sudo apt install wps-office wps-office-language-all-pt wps-office-language-all-en wps-office-language-all-fr
